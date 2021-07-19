d15a90b13bc189efb1a785b4052bd6cdc4e5c42c
kemei-8 <84368186+kemei-8@users.noreply.github.com>
2021-07-19 03:47:30 -0500

    A twist on the classic 8ball.

--- Ask Me Anything/ViewController.swift
+++ Ask Me Anything/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5", "ball6"]
+    var randomBallNumber = 0
+
+    @IBOutlet weak var askBallImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0...5)
+        askBallImage.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
+    
 }
 
